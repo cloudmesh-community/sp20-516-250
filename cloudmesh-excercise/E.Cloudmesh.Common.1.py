596f08a3187bd962835a77b4fd7f69dcd192839d
psendil <psendil@gmail.com>
2020-02-16 15:22:13 -0600

    Create E.Cloudmesh.Common.1.py
    common.1.py

---
+++ cloudmesh-excercise/E.Cloudmesh.Common.1.py
@@ -0,0 +1,12 @@
+# sp20-516-250 E.Cloudmesh.Common.1
+
+from cloudmesh.common.util import banner
+banner("This is Senthil banner")
+
+from cloudmesh.common.util import HEADING
+HEADING("Spring 2020 Cloud Computing Class")
+print("Demo heading")
+
+from cloudmesh.common.debug import VERBOSE
+m = {"first_name": "Senthil Kumar"}
+VERBOSE(m)
